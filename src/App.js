29a1a8fafcfad69b3a1e2d9f68429c8ea7d1cc4a
HarryPirate <troncosokristoffer@gmail.com>
2018-08-06 14:22:50 -0400

    Change h1 in App component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello my name is KT, welcome to my App!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
